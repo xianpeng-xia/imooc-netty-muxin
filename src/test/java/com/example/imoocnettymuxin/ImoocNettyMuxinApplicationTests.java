c9268918239e399422bdaa2966f5bf07f880b5b4
xianpeng.xia <951256916>
2019-07-05 22:14:34 +0800

    init

---
+++ src/test/java/com/example/imoocnettymuxin/ImoocNettyMuxinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.imoocnettymuxin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImoocNettyMuxinApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
